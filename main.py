57f6e6caf70500de1187bdba99acc0b17c12869e
Tim Helfensdörfer <thelfensdrfer@gmail.com>
2024-01-20 17:32:48 +0100

    fix: feed syntax

--- main.py
+++ main.py
@@ -138,8 +138,8 @@ async def refresh_rss():
         )
 
     # Convert event list to RSS
-    rss = f"""<?xml version="1.0" encoding="UTF-8" xmlns:atom="http://www.w3.org/2005/Atom">
-    <rss version="2.0">
+    rss = f"""<?xml version="1.0" encoding="UTF-8" ?>
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
         <channel>
             <title>Wuppertal Live</title>
             <link>https://www.wuppertal-live.de</link>
